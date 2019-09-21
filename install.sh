8e10fece4733f40c83a3d0255d026dff8a2ffe31
phillipgraniero <phillipgraniero@users.noreply.github.com>
2019-09-21 08:42:26 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ echo NodeJS status = $isNode
 
 if [ $isNode -eq 0 ]; then
   node -e "process.exit(Number(process.version.substr(1).split('.')[0]) > 5 ? 0 : 1)"
-  isNode=$?
+  isNode=$?cd "$(dirname "$0")/app"
 fi
 if [ $isNode -eq 0 ]; then
   echo "Installer is using your system NodeJS."
@@ -22,3 +22,4 @@ else
     ../node/x86/node install.js --add_node $1
   fi
 fi
+cd "$(dirname "$0")/app"
